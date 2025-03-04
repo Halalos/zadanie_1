cd344b83f23a604ce11540779b46f131732e0bcc
Halalos <balogh.laszlo.andras@gmail.com>
2025-03-04 15:12:28 +0100

    Prvý - Template

---
+++ partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="tm-footer">
+        <span>Copyright &copy; 2020 Your Company</span>
+        <span>Web Designed by 
+        <a href="https://templatemo.com" title="free website templates" target="_parent" rel="nofollow">TemplateMo</a></span>
+</footer>
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OCEAN vibes by TemplateMo</title>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"> <!-- https://fonts.google.com/ -->
+    <link rel="stylesheet" href="fontawesome/css/all.min.css"> <!-- https://fontawesome.com/ -->
+    <link rel="stylesheet" href="css/magnific-popup.css"> 
+    <link rel="stylesheet" href="css/templatemo-ocean-vibes.css"> 
+<!--
+    
+TemplateMo 554 Ocean Vibes
+
+https://templatemo.com/tm-554-ocean-vibes
+
+-->
+</head>
+<body>
+    <header class="tm-site-header">
+        <h1 class="tm-mt-0 tm-mb-15"><span class="tm-color-primary">OCEAN</span> <span class="tm-color-gray-2">vibes</span></h1>
+        <em class="tm-tagline tm-color-light-gray">simple and effective design</em>
+    </header>
